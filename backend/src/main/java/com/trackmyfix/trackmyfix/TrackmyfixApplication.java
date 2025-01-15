00ac2dd8793bb147ec7c0cf6e7bfd930bbd8c781
guspaz0 <gusti.paz@gmail.com>
2025-01-15 12:50:31 -0300

    chore project initialization

---
+++ backend/src/main/java/com/trackmyfix/trackmyfix/TrackmyfixApplication.java
@@ -0,0 +1,13 @@
+package com.trackmyfix.trackmyfix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackmyfixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackmyfixApplication.class, args);
+	}
+
+}
