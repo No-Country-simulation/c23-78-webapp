0809570d91d9c8ab2242228f02122feb7f42ce75
Dennis Ponce <denizponce16@gmail.com>
2025-01-28 14:07:17 -0500

    modified index.js barrel

--- frontend/src/modules/core/components/index.js
+++ frontend/src/modules/core/components/index.js
@@ -3,5 +3,4 @@ export * from './testimonial/TestimonialContainer';
 export * from './testimonial/TestimonialCard';
 export * from './footer/Footer';
 export * from './notFound/DeviceNotFound';
-export * from './orderForm/OrderForm';
 export * from './tracking/TrackingTimeLine';
--- frontend/src/pages/Principal.jsx
+++ frontend/src/pages/Principal.jsx
@@ -3,7 +3,8 @@ import OurServices from "../modules/core/components/OurServices/OurServices";
 import AboutUs from "../modules/core/components/About/AboutUs";
 import ContactForm from "../modules/core/components/Form/ContactForm";
 import { Link } from "react-router-dom";
-import { Footer, NavBar, TestimonialContainer, DeviceNotFound, TrackingTimeLine, OrderForm } from "../modules/core/components";
+import { Footer, NavBar, TestimonialContainer, DeviceNotFound, TrackingTimeLine } from "../modules/core/components";
+import { OrderForm } from "../modules/core/components/OrderForm/OrderForm";
 
 
 const Principal = () => {
