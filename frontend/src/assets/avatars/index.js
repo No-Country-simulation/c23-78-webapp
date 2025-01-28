eb5288e9c92ecad660e38c8a42455d34b960c18c
guspaz0 <gusti.paz@gmail.com>
2025-01-28 20:05:39 -0300

    Revert "merge branch developer, fix: .gitignore"
    This reverts commit 4c2e1a3d195ea6bad41fc6de3f245913fceb9503.

--- frontend/src/assets/avatars/index.js
+++
@@ -1,6 +0,0 @@
-import profile1 from './profile1.jpg'
-import profile2 from './profile2.jpg'
-import profile3 from './profile3.jpg'
-import profile4 from './profile4.jpg'
-
-export {profile1, profile2, profile3, profile4}; 
