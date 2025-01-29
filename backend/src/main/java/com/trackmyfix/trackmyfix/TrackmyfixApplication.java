a7a03c66daf22f72eff2aec8a375f44b61f74946
guspaz0 <gusti.paz@gmail.com>
2025-01-29 10:07:42 -0300

    fix: merging frontend develop

--- backend/src/main/java/com/trackmyfix/trackmyfix/TrackmyfixApplication.java
+++ backend/src/main/java/com/trackmyfix/trackmyfix/TrackmyfixApplication.java
@@ -3,6 +3,8 @@ package com.trackmyfix.trackmyfix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//comment to delete after merging to develop, for fixing merging.
+
 @SpringBootApplication
 public class TrackmyfixApplication {
 
