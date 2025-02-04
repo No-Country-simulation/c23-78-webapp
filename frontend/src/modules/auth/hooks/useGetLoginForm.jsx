5db13667c1711ffa16f4408fa26a900c1e4c0315
lorenzosegada <lorenzosegada@gmail.com>
2025-02-03 21:25:53 -0300

    Modificacion del usegetlogin

--- frontend/src/modules/auth/hooks/useGetLoginForm.jsx
+++ frontend/src/modules/auth/hooks/useGetLoginForm.jsx
@@ -1,34 +1,38 @@
+import { useForm } from "react-hook-form";
+import authLoginUser from "../services/authLoginUser";
 import { saveTokens } from "../libs/tokenStorage";
 
-/**
- * Hook personalizado para manejar el formulario de inicio de sesión.
- * @param {Object} defaultValues - Valores predeterminados para el formulario.
- * @returns {Object} - Métodos y estados del formulario.
- */
 const useGetLoginForm = (defaultValues) => {
     const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        reset,
+        watch,
     } = useForm({
+        defaultValues, // valores predeterminados
     });
 
-    /**
-     * Función que se ejecuta al enviar el formulario.
-     * @param {Object} data - Contiene los valores ingresados en el formulario.
-     */
+    // Manejo del
     const onSubmit = (data) => {
         (async () => {
             try {
-
-                // Llamamos a la función de autenticación con los datos ingresados
+                console.log("enviando, data", data);
                 const result = await authLoginUser(data.email, data.password);
                 saveTokens(result);
-                // Aquí podríamos manejar la respuesta del login, como guardar tokens o redirigir
             } catch (error) {
-                console.error("Error en el inicio de sesión:", error);
+                console.error("Login failed:", error);
             }
         })();
     };
 
     return {
+        register, // conectar los inputs
+        handleSubmit, // manejo de informacion
+        onSubmit, // submit del form
+        errors, // catch para errores
+        reset, // Caso reinicio
+        watch, // Trigger 
     };
 };
 
